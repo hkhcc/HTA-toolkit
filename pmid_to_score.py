7334fefab1f1060db79eeae53e8213ff2eb5e20f
hkhcc <29171587+hkhcc@users.noreply.github.com>
2021-03-26 16:21:25 +0800

    Update pmid_to_score.py

--- pmid_to_score.py
+++ pmid_to_score.py
@@ -10,16 +10,6 @@ import sys
 from ncbi_search import load_abstract
 from UnifiedQA import preprocess_input, run_model
 
-sign_grading_question = '''
-This paper is a _ \n
-(A) Meta-analysis, systematic review of randomized controlled trials (RCTs), or randomized controlled trial
-(B) Systematic review of case control or cohort studies; case control or cohort study
-(C) Case report or case series
-(D) Expert opinion
-(E) Cannot be determined
-'''
-
-
 pmid_list = ['29744625', '30675655']
 abstracts = load_abstract(pmid_list, separate_title=False)
 
@@ -27,7 +17,12 @@ for abstract in abstracts:
     print(abstract, file=sys.stderr)
     pred = run_model('What is this study about?' + '\\n' + preprocess_input(abstract[1]))
     print('Summary:', pred, file=sys.stderr)
-    pred = run_model(preprocess_input(sign_grading_question) + '\\n' + preprocess_input(abstract[1]))
-    print('Prediction:', pred, file=sys.stderr)
+    pred = run_model('Is this a meta-analysis or systematic review or randomized controlled trial?' + '\\n' + preprocess_input(abstract[1]))
+    print('Grade 1:', pred, file=sys.stderr)
+    pred = run_model('Is this a case-control or cohort study?' + '\\n' + preprocess_input(abstract[1]))
+    print('Grade 2:', pred, file=sys.stderr)
+    pred = run_model('Is this a case series or case report?' + '\\n' + preprocess_input(abstract[1]))
+    print('Grade 3:', pred, file=sys.stderr)
+
 
 
