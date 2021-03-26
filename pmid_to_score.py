445e4b1e8f43a99d82c50f0d0cfb444e2168e8d3
hkhcc <29171587+hkhcc@users.noreply.github.com>
2021-03-26 16:24:56 +0800

    Update pmid_to_score.py

--- pmid_to_score.py
+++ pmid_to_score.py
@@ -14,14 +14,15 @@ pmid_list = ['29744625', '30675655']
 abstracts = load_abstract(pmid_list, separate_title=False)
 
 for abstract in abstracts:
+    abstract[1] = preprocess_input(abstract[1])
     print(abstract, file=sys.stderr)
-    pred = run_model('What is this study about?' + '\\n' + preprocess_input(abstract[1]))
+    pred = run_model('What is this study about?' + '\\n' + abstract[1])
     print('Summary:', pred, file=sys.stderr)
-    pred = run_model('Is this a meta-analysis or systematic review or randomized controlled trial?' + '\\n' + preprocess_input(abstract[1]))
+    pred = run_model('Is this a meta-analysis or systematic review or randomized controlled trial?' + '\\n' + abstract[1])
     print('Grade 1:', pred, file=sys.stderr)
-    pred = run_model('Is this a case-control or cohort study?' + '\\n' + preprocess_input(abstract[1]))
+    pred = run_model('Is this a case-control or cohort study?' + '\\n' + abstract[1])
     print('Grade 2:', pred, file=sys.stderr)
-    pred = run_model('Is this a case series or case report?' + '\\n' + preprocess_input(abstract[1]))
+    pred = run_model('Is this a case series or case report?' + '\\n' + abstract[1])
     print('Grade 3:', pred, file=sys.stderr)
 
 
